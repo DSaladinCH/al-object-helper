163832e24dedd531ef668772334073e98e0b5cb9
Dominic Saladin <dominic@dsaladin.ch>
2021-05-05 15:11:44 +0200

    Updated gitignore

--- out/AppType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppType = void 0;
-var AppType;
-(function (AppType) {
-    AppType[AppType["local"] = 0] = "local";
-    AppType[AppType["appPackage"] = 1] = "appPackage";
-})(AppType = exports.AppType || (exports.AppType = {}));
-//# sourceMappingURL=AppType.js.map
